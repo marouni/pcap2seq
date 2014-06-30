57eef33e26284c8abaa49e749282bb0787511a44
marouni <abbass.marouni@gmail.com>
2014-06-30 21:53:27 +0200

    Used Reflection for instantiating a codec

--- src/main/java/io/ushabti/SequenceFileWriter.java
+++ src/main/java/io/ushabti/SequenceFileWriter.java
@@ -2,7 +2,6 @@ package io.ushabti;
 
 import java.io.IOException;
 import java.net.URI;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -10,8 +9,8 @@ import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
-import org.apache.hadoop.io.compress.BZip2Codec;
-import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.util.ReflectionUtils;
 
 
 public class SequenceFileWriter {
@@ -22,20 +21,20 @@ public class SequenceFileWriter {
 	
 	private IntWritable key = new IntWritable();
 	private BytesWritable value = new BytesWritable();
-	private BZip2Codec bzip2Codec;
-	private GzipCodec gzipCodec;
 	
 	private SequenceFile.Writer writer = null;
 	
-	public SequenceFileWriter(String sequenceFile, String compressionSupport) throws IOException{
+	public SequenceFileWriter(String sequenceFile, String compressionCodec) throws IOException, ClassNotFoundException{
 		configuration = new Configuration();
 		fileSystem = FileSystem.get(URI.create(sequenceFile), configuration);
 		path = new Path(sequenceFile);
 		
-		if (compressionSupport.equalsIgnoreCase("gzip"))
-			writer = SequenceFile.createWriter(fileSystem, configuration, path,key.getClass(), value.getClass(), org.apache.hadoop.io.SequenceFile.CompressionType.BLOCK, gzipCodec);
-		else if (compressionSupport.equalsIgnoreCase("bzip2"))
-			writer = SequenceFile.createWriter(fileSystem, configuration, path,key.getClass(), value.getClass(), org.apache.hadoop.io.SequenceFile.CompressionType.BLOCK, bzip2Codec);
+		
+		if (compressionCodec.equalsIgnoreCase("org.apache.hadoop.io.compress.BZip2Codec")||compressionCodec.equalsIgnoreCase("org.apache.hadoop.io.compress.GzipCodec")) {
+			Class<?> codecClass = Class.forName(compressionCodec);
+			CompressionCodec codec = (CompressionCodec) ReflectionUtils.newInstance(codecClass, configuration);
+			writer = SequenceFile.createWriter(fileSystem, configuration, path,key.getClass(), value.getClass(), org.apache.hadoop.io.SequenceFile.CompressionType.BLOCK, codec);
+		}
 		else
 			writer = SequenceFile.createWriter(fileSystem, configuration, path,key.getClass(), value.getClass());	
 	}
